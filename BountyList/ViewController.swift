6a3c6d3bbba48a4a496339bf2f8428c490c0e29e
mellow0410@naver.com <mellow0410@gmail.com>
2020-08-31 20:27:51 +0900

    Initial Commit

---
+++ BountyList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by SN on 2020/08/31.
+//  Copyright © 2020 com.snkim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
